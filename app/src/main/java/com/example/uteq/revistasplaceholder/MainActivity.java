84d069121976fab75968fd36ba5b204774df000d
Jose Garcia <jgarcia24121996@gmail.com>
2021-03-11 17:10:41 -0500

    read josn revistas

--- app/src/main/java/com/example/uteq/revistasplaceholder/MainActivity.java
+++ app/src/main/java/com/example/uteq/revistasplaceholder/MainActivity.java
@@ -5,36 +5,53 @@ import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.os.Bundle;
 
+import com.example.uteq.revistasplaceholder.model.Revista;
 import com.example.uteq.revistasplaceholder.webservice.Asynchtask;
+import com.example.uteq.revistasplaceholder.webservice.WebService;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.mindorks.placeholderview.PlaceHolderView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity implements Asynchtask {
     PlaceHolderView phvGallery;
+    Type revistaType;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         phvGallery = (PlaceHolderView) findViewById(R.id.phv_gallery);
 
+        Map<String, String> datos = new HashMap<String, String>();
+        WebService ws = new WebService("https://revistas.uteq.edu.ec/ws/journals.php", datos, this, this);
+        ws.execute("");
+
+
 // (Optional): If customization is Required then use Builder with the PlaceHolderView
         phvGallery.getBuilder()
                 .setHasFixedSize(false)
                 .setItemViewCacheSize(10)
                 .setLayoutManager(new GridLayoutManager(this, 3));
+        revistaType = new TypeToken<List<Revista>>() {
+        }.getType();
 
-//        phvGallery
-//                .addView(new GalleryImage(getApplicationContext(), url1))
-//                .addView(new GalleryImage(getApplicationContext(), url2))
-//                .addView(new GalleryImage(getApplicationContext(), url3))
-//                .addView(new GalleryImage(getApplicationContext(), url4));
     }
 
     @Override
     public void processFinish(String result) throws JSONException {
-        JSONObject pais=  new JSONObject(result);
-
+        ArrayList<Revista> revistas = new Gson().fromJson(result, revistaType);
+        for (Revista rev : revistas) {
+            phvGallery.addView(new GalleryImage(getApplicationContext(), rev.getPortada()));
+        }
     }
 }
--- app/src/main/java/com/example/uteq/revistasplaceholder/model/Revista.java
+++ app/src/main/java/com/example/uteq/revistasplaceholder/model/Revista.java
@@ -9,4 +9,75 @@ public class Revista {
     private String journalThumbnail;
     private String name;
 
+    public Revista() {
+    }
+
+    public Revista(String journal_id, String portada, String abbreviation, String description, String journalThumbnail, String name) {
+        this.journal_id = journal_id;
+        this.portada = portada;
+        this.abbreviation = abbreviation;
+        this.description = description;
+        this.journalThumbnail = journalThumbnail;
+        this.name = name;
+    }
+
+    public String getJournal_id() {
+        return journal_id;
+    }
+
+    public void setJournal_id(String journal_id) {
+        this.journal_id = journal_id;
+    }
+
+    public String getPortada() {
+        return portada;
+    }
+
+    public void setPortada(String portada) {
+        this.portada = portada;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public void setAbbreviation(String abbreviation) {
+        this.abbreviation = abbreviation;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getJournalThumbnail() {
+        return journalThumbnail;
+    }
+
+    public void setJournalThumbnail(String journalThumbnail) {
+        this.journalThumbnail = journalThumbnail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Revista{" +
+                "journal_id='" + journal_id + '\'' +
+                ", portada='" + portada + '\'' +
+                ", abbreviation='" + abbreviation + '\'' +
+                ", description='" + description + '\'' +
+                ", journalThumbnail='" + journalThumbnail + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
